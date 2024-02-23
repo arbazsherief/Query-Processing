7a7b01a434aa86a644052930cd94d039fa91357c
arbazsherief <113408671+arbazsherief@users.noreply.github.com>
2024-02-23 09:13:39 +0530

    Add files via upload

---
+++ Exp28_horizontal_bar_chart.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+languages = ['Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++']
+popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
+
+plt.figure(figsize=(8, 6))
+
+plt.barh(languages, popularity, color='skyblue')
+
+plt.xlabel('Popularity')
+plt.ylabel('Programming Languages')
+plt.title('Popularity of Programming Languages')
+
+plt.tight_layout()
+plt.show()
